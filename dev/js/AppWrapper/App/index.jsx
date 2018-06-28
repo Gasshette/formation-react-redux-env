fe9779f72e22fda09a771df3aa867ae7290c173d
Quentin GRISEL <quentin.grisel@capgemini.com>
2018-06-28 14:02:48 +0200

    Edit/Clean documentation. Match the code with the documentation.

--- dev/js/AppWrapper/App/index.jsx
+++ dev/js/AppWrapper/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Hello from '../../Containers/Hello';
+import Hello from '../../Components/Hello';
 
 const App = () => <Hello />;
 
--- dev/js/index.jsx
+++ dev/js/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './Components/Hello';
+import App from './AppWrapper/App';
 
 ReactDOM.render(
-  <Hello />,
+  <App />,
   document.querySelector('#root'),
 );
 
